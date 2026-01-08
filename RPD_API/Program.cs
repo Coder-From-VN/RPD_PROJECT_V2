78656f93947c51972454a777acc775f983c933a0
NH.Chien <nhchienfjo@gmail.com>
2026-01-08 10:03:01 +0700

    UpdatePreModifileCode

--- RPD_API/Program.cs
+++ RPD_API/Program.cs
@@ -49,11 +49,11 @@ builder.Services.AddDbContext<rpdDbContext>(op =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 //app.UseSwaggerUI(c =>
 //{
 //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
