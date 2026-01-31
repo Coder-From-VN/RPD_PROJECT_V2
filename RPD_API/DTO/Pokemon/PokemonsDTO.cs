97d3349884b4c173d88055973034d8438023bb7d
NH.Chien <nhchienfjo@gmail.com>
2026-01-31 20:15:54 +0700

    update dto

--- RPD_API/DTO/Pokemon/PokemonsDTO.cs
+++ RPD_API/DTO/Pokemon/PokemonsDTO.cs
@@ -6,9 +6,8 @@ namespace RPD_API.DTO
     {
         public Guid pokeID { get; set; }
         public int pokeNationalNumber { get; set; }
-        public string pokeDescription { get; set; }
         public string pokeName { get; set; }
-        public string pokeSpecies { get; set; }
+
         //Image 
         public ICollection<ImageLinkDTO> ImageLink { get; set; }
         //PokemonType 
